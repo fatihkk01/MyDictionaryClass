f6ccdb46000b475068a4a50cb36f2ce2e843182a
Fatih Kırık <fatih.kirik@bilgiedu.net>
2021-01-23 19:48:14 +0300

    4. Ders Dictionary Ödevi

--- MyDictionaryClass/Program.cs
+++ MyDictionaryClass/Program.cs
@@ -9,7 +9,7 @@ namespace MyDictionaryClass
             GameDictionary<int, string, string, int, int> games = new GameDictionary<int, string, string, int, int>();
             games.Add(1,"CyberPunk 2077", "10 December 2020",250,9);
             games.Add(2,"Counter Strike Global Offensive", "21 December 2012",80,7);
-            games.Add(3,"FIFA 21", "5 October 2020", 80,7);
+            games.Add(3,"FIFA 21", "5 October 2020", 280,7);
             games.Read();
         }
     }
